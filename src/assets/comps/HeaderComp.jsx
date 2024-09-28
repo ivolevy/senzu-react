605a64ea44496219e02f4300a706938c29bb4227
ivan levy <ivo.levy03@gmail.com>
2024-09-28 17:02:40 -0300

    changing header

--- src/assets/comps/HeaderComp.jsx
+++ src/assets/comps/HeaderComp.jsx
@@ -1,6 +1,16 @@
 export const HeaderComp = () => {
   return (
    <section className="headerComp">
+    <h1 className="title">
+      Tu futuro digital empieza aquí
+    </h1>
+    <p className="">
+      En Senzu solo diseñamos. Revolucionamos. <br />
+      Creamos experiencias digitales que no solo cuentan tu historia, 
+      sino que la hacen inolvidable. <br />
+      Tu marca, nuestra visión: un futuro sin límites.
+    </p>
+    <a href="" className="button">Vamos a hacerlo</a>
    </section>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        tanNimbus: ['TAN Nimbus', 'sans-serif'],
+      },
+    },
   },
   plugins: [require('daisyui')],
 }
